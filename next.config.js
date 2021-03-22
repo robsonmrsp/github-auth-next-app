dc46fa2f2823fd68aac239ae3ce542d95972b56f
Marcio Robson <robsonmrsp@gmail.com>
2021-03-22 00:32:40 -0300

    Chamando o end-point para remover a estrela de um repo.  - Pequenas alterações no desenho do componente CardRepo.  - Alterado o servico de usuário/estrela(ainda um rascunho) para chamar os endpoints do github
    Nota:
       Problemas na configuração da chave dos dois mecanismos de autenticação causaram perda de tempo.

---
+++ next.config.js
@@ -0,0 +1,7 @@
+// module.exports = {
+//   env: {
+//     APP_CLIENT_ID: '85dfab3a30545c6fe907',
+//     APP_CLIENT_SECRET: 'b1a9a6f0387154fe8bb2543e5464b0613f9a66f4',
+//     APP_REDIRECT_URI: 'http://localhost:3000/validate',
+//   },
+// };
