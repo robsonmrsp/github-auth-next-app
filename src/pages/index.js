65b2db5ec5c295773a53033598c0864d4b1fe78d
Marcio Robson <robsonmrsp@gmail.com>
2021-03-23 01:27:39 -0300

    Refactory  - Criado o componente de layout que abrigará a lógida de busca do usuário github que se comportará de maneiras distintas para usuário loggedIn e guest.
    Notas:
     - Aplicar alguma melhoria na tela home que é protegida.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,74 +1,28 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, useContext } from 'react';
 import { faUsers, faLink } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { MapContainer } from '@/shared/MapConainer';
-import SearchField from '@/components/SearchField';
 import CardUser from '@/components/CardUser';
+import Layout from '@/components/Layout';
+import { AppContext } from '@/shared/AppContext';
 
 export default function Home() {
-  const [user, setUser] = useState();
-  const [starredRepos, setStarredRepos] = useState([]);
-  const [fetchUserError, setFetchUserError] = useState(false);
-  const [loading, setLoading] = useState(false);
-
-  const searchUser = async (loginName) => {
-    setLoading(true);
-    setFetchUserError(false);
-    setUser(null);
-
-    if (loginName) {
-      await fetchUser(loginName);
-      fetchStarred(loginName);
-    }
-    setLoading(false);
-  };
-
-  const fetchUser = async (userName) => {
-    const response = await fetch(`https://api.github.com/users/${userName}`);
-    if (!response.ok) {
-      setFetchUserError(true);
-    } else {
-      const user = await response.json();
-      setFetchUserError(false);
-      setUser(user);
-    }
-  };
-
-  const fetchStarred = async (userName) => {
-    const resp = await fetch(`https://api.github.com/users/${userName}/starred`);
-    console.log(1);
-    console.log('resp.ok', resp.ok);
-    if (resp.ok) {
-      console.log(2);
-      const starRep = await resp.json();
-      console.log(3);
-      setStarredRepos(starRep);
-    }
-    console.log(4);
-  };
+  const { state } = useContext(AppContext);
 
   return (
-    <>
-      <>
-        <section className="hero">
-          <div className="hero-body">
-            <p className="title">GitHub App</p>
-            <SearchField placeholder="Find a repository" onFetchUser={searchUser} />
-          </div>
-        </section>
-        {user && (
-          <div className="container is-fullhd">
-            <div className="columns">
-              <div className="column is-4">
-                <CardUser user={user} starredRepos={starredRepos} />
-              </div>
-              <div className="column is-8">
-                <MapContainer location={user.location} />
-              </div>
+    <Layout>
+      {state.user && (
+        <div className="container is-fullhd">
+          <div className="columns">
+            <div className="column is-4">
+              <CardUser user={state.user} starredRepos={state.starredRepos} />
+            </div>
+            <div className="column is-8">
+              <MapContainer location={state.user.location} />
             </div>
           </div>
-        )}
-      </>
-    </>
+        </div>
+      )}
+    </Layout>
   );
 }
