6b1c7277721fe8b37ebc83aa597e3126c3587d65
Marcio Robson <robsonmrsp@gmail.com>
2021-03-23 12:15:05 -0300

    Refactory fetch  - Alterado/criado servicos  para que todos os requests passarem pelo nosso server. Esse é o primeiro passo para tentar unificar os componentes AuthLayout e Layout.
    Nota:
     - Na preocupação com a funcionalidade ainda os tratamentos de erro e sinalizacao para o usuário de que algo está sendo processando
    ficaram para depois. Precisamos tratar os casos de erro e loading.

--- src/config/Constants.js
+++ src/config/Constants.js
@@ -1,4 +1,4 @@
 export const clientId = process.env.APP_CLIENT_ID;
 export const redirectUri = process.env.APP_REDIRECT_URI;
 export const clientSecret = process.env.APP_CLIENT_SECRET;
-export const proxyUrl = process.env.APP_PROXY_URL;
+export const API_URL = process.env.API_URL;
