2476cf66c49675834001f21d1524c41065d62c27
Marcio Robson <robsonmrsp@gmail.com>
2021-03-23 21:34:46 -0300

    Correção do endereço dos repos

--- src/pages/api/guest/[username]/starreds/index.js
+++ src/pages/api/guest/[username]/starreds/index.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 const getStarredRepos = async (req, res) => {
   const { username } = req.query;
 
-  const response = await fetch(`https://api.github.com/users/${username}/starreds`);
+  const response = await fetch(`https://api.github.com/users/${username}/starred`);
 
   const json = await response.json();
   res.status(response.status).json(json);
